6230e534a308520239b7dbf72c5baea63b52211d
sachinbareth <sacinbareth099@gmail.com>
2025-11-14 21:11:55 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sachin Bareth",
+    author_email="sachinbareth099@gmail.com",
+    packages=find_packages()
+)
